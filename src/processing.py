b5efdf9d6da62963e130d1aeb94899ddccc6d2da
Pavel Anikanov <chef88ad@gmail.com>
2025-06-29 15:29:55 +0300

    fixed variable names

--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,17 @@
-def filter_by_state(user_ids: list, state: str = "EXECUTED") -> list:
+def filter_by_state(user_id_list: list, state: str = "EXECUTED") -> list:
     """Возвращает новый список словарей, содержащий только те словари, у которых ключ
     state соответствует указанному значению"""
 
-    filt_users = []
-    for user_id in user_ids:
+    filter_user = []
+    for user_id in user_id_list:
         if user_id['state'] == state:
-            filt_users.append(user_id)
+            filter_user.append(user_id)
         else:
             continue
-    return filt_users
+    return filter_user
 
 
-def sort_by_date(user_ids: list, reverse: bool = False) -> list:
+def sort_by_date(user_id_list: list, reverse: bool = False) -> list:
     """Возвращает новый список, отсортированный по дате"""
 
-    return sorted(user_ids, key=lambda user: user['date'], reverse=reverse)
+    return sorted(user_id_list, key=lambda user: user['date'], reverse=reverse)
