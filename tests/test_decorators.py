64a8738d2c401432ec2cf4e4641f1caeb785fc48
Pavel Anikanov <chef88ad@gmail.com>
2025-07-25 11:49:12 +0300

    Fixed some mistakes

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -3,27 +3,10 @@ import pytest
 from src.decorators import log
 
 
-@log
-def successful_function(x, y):
-    return x + y
+@log()
+def test_function(x=10):
+    assert x * 2 == 20
 
-
-@log
-def failing_function(x, y):
-    return x / y
-
-
-def test_successful_function(capsys):
-    result = successful_function(1, 2)
-    captured = capsys.readouterr()
-
-    assert result == 3
-    assert "successful_function ok" in captured.out
-
-
-def test_failing_function(capsys):
-    with pytest.raises(ZeroDivisionError):
-        failing_function(1, 0)
-
-    captured = capsys.readouterr()
-    assert "failing_function error: ZeroDivisionError. Inputs: (1, 0), {}" in captured.out
+@log("test_log.txt")
+def test_function_with_logging(x=1):
+    assert x + 1 == 2
